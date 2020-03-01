0e64feb73ceccaa076a5699e640dbacf19926a46
anupritaisno1 <www.anuprita804@gmail.com>
2020-03-01 13:41:06 +0000

    common: change display options to match glassrom policy changes disable DRM. It is unethical https://www.defectivebydesign.org/ https://fckdrm.com/ Enable HDR and wide color display
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- common.mk
+++ common.mk
@@ -243,12 +243,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     OnePlusDoze
 
-# DRM
-PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.2-service.clearkey
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.oneplus_msmnile \
