b5e8842b699001ffaf325ba1ce442c1a596cfec2
anupritaisno1 <www.anuprita804@gmail.com>
2020-03-25 02:23:51 +0000

    sm8150: build qti-telephony-common
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- common.mk
+++ common.mk
@@ -433,6 +433,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ims-ext-common \
     ims_ext_common.xml \
+    qti-telephony-common \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
