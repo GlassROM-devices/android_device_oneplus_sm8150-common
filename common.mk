11eed6d21954378a23f616090bac11c6e0609a8f
anupritaisno1 <www.anuprita804@gmail.com>
2020-05-01 03:02:25 +0200

    Revert "sm8150-common: nuke otapreopt script"
    This reverts commit 1c3d11cabccedae79117c1d8775e376214d490eb.

--- common.mk
+++ common.mk
@@ -37,12 +37,19 @@ AB_OTA_PARTITIONS += \
     vbmeta
 
 AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \
+    otapreopt_script \
     checkpoint_gc
 
 # Permissions
